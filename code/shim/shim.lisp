ac7bc4b2a18d7b28728b8725ee6e5acad495b240
Tarn W. Burton <twburton@gmail.com>
2023-06-14 06:35:20 -0400

    Add with-standard-io-syntax support

--- code/shim/shim.lisp
+++ code/shim/shim.lisp
@@ -4,3 +4,20 @@
   ())
 
 (setf inravina-intrinsic:*client* (make-instance 'shim-client))
+
+;;; The following hacks are all to make WITH-STANDARD-IO-SYNTAX work.
+
+#+(or clasp ecl)
+(setf (first (cdr si::+io-syntax-progv-list+)) inravina-intrinsic:*standard-pprint-dispatch*)
+
+#+ecl
+(setf (first (cdr si::+ecl-syntax-progv-list+)) inravina-intrinsic:*standard-pprint-dispatch*)
+
+#+ccl
+(setf ccl::*standard-pprint-dispatch-table* inravina-intrinsic:*standard-pprint-dispatch*)
+
+#+sbcl
+(setf sb-pretty::*standard-pprint-dispatch-table* inravina-intrinsic:*standard-pprint-dispatch*)
+
+#+sbcl
+(setf pp::*initial-pprint-dispatch* inravina-intrinsic:*standard-pprint-dispatch*)
