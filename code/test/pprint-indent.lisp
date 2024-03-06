9a1a361b5653bf4480981b6fdbdd4cdfc478ebf2
Tarn W. Burton <twburton@gmail.com>
2024-03-06 12:14:02 -0500

    Add more tests

--- code/test/pprint-indent.lisp
+++ code/test/pprint-indent.lisp
@@ -20,7 +20,7 @@ ZC[D
 Z  E
 ZF]"
       (with-env (stream)
-        (inravina:pprint-logical-block (*client* stream  nil :per-line-prefix "Z")
+        (inravina:pprint-logical-block (*client* stream nil :per-line-prefix "Z")
           (write-string "A " stream)
           (inravina:pprint-newline *client* stream :mandatory)
           (write-string "B
@@ -30,3 +30,60 @@ C" stream)
             (inravina:pprint-newline *client* stream :mandatory)
             (write-string "E
 F" stream))))))
+
+(define-test pprint-logical-block.1
+  (is equal
+      "a .."
+      (with-env (stream :lines 1)
+        (inravina:pprint-logical-block (*client* stream nil :prefix "a
+(" :suffix ")")
+          (write-string "wibble" stream)
+          (inravina:pprint-newline *client* stream :mandatory)
+          (write-string "bar" stream)))))
+
+(define-test pprint-logical-block.2
+  (is equal
+      "a
+(wibble ..)"
+      (with-env (stream :lines 2)
+        (inravina:pprint-logical-block (*client* stream nil :prefix "a
+(" :suffix ")")
+          (write-string "wibble" stream)
+          (inravina:pprint-newline *client* stream :mandatory)
+          (write-string "bar" stream)))))
+
+(define-test pprint-logical-block.3
+  (is equal
+      "za .."
+      (with-env (stream :lines 1)
+        (inravina:pprint-logical-block (*client* stream nil :per-line-prefix "z")
+          (inravina:pprint-logical-block (*client* stream nil :prefix "a
+(" :suffix ")")
+            (write-string "wibble" stream)
+            (inravina:pprint-newline *client* stream :mandatory)
+            (write-string "bar" stream))))))
+
+(define-test pprint-logical-block.4
+  (is equal
+      "za ..
+z(wibble ..)"
+      (with-env (stream :lines 2)
+        (inravina:pprint-logical-block (*client* stream nil :per-line-prefix "z")
+          (inravina:pprint-logical-block (*client* stream nil :prefix "a
+(" :suffix ")")
+            (write-string "wibble" stream)
+            (inravina:pprint-newline *client* stream :mandatory)
+            (write-string "bar" stream))))))
+
+(define-test pprint-logical-block.5
+  (is equal
+      "[a
+(wibble
+ bar)]"
+      (with-env (stream :lines 2)
+        (inravina:pprint-logical-block (*client* stream nil :prefix "[" :suffix "]")
+          (inravina:pprint-logical-block (*client* stream nil :prefix "a
+(" :suffix ")")
+            (write-string "wibble" stream)
+            (inravina:pprint-newline *client* stream :mandatory)
+            (write-string "bar" stream))))))
--- code/test/utilities.lisp
+++ code/test/utilities.lisp
@@ -1,7 +1,23 @@
 (in-package #:inravina/test)
 
-(defmacro with-env ((stream &key right-margin) &body body)
-  `(let ((*print-right-margin* ,right-margin))
+(defmacro with-env ((stream
+                     &key (array t) (base 10) (case :upcase) circle
+                          (escape t) (gensym t) level length lines
+                          miser-width (pretty t) readably right-margin)
+                    &body body)
+  `(let ((*print-array* ,array)
+         (*print-base* ,base)
+         (*print-case* ,case)
+         (*print-circle* ,circle)
+         (*print-escape* ,escape)
+         (*print-gensym* ,gensym)
+         (*print-level* ,level)
+         (*print-length* ,length)
+         (*print-lines* ,lines)
+         (*print-miser-width* ,miser-width)
+         (*print-pretty* ,pretty)
+         (*print-readably* ,readably)
+         (*print-right-margin* ,right-margin))
      (with-output-to-string (,stream)
        ,@body)))
 
@@ -29,7 +45,7 @@
 (defmethod incless:write-object ((client test-client) object stream)
   (if *print-pretty*
       (funcall (inravina:pprint-dispatch client *print-pprint-dispatch* object) stream object)
-      (call-next-method)))
+      (write object :stream stream)))
 
 (defmethod incless:print-object ((client test-client) object stream)
   (declare (ignore client))
