a1c98c87efee5a8b5d244d116a8520981c252fff
Tarn W. Burton <twburton@gmail.com>
2026-04-01 10:19:59 -0400

    Use make-instance for now

--- code/intrinsic/interface.lisp
+++ code/intrinsic/interface.lisp
@@ -4,7 +4,7 @@
 
 (defclass client-impl (client quaviver/schubfach:client) ())
 
-(change-class incless-intrinsic:*client* 'client-impl)
+(setf incless-intrinsic:*client* (make-instance 'client-impl))
 
 (inravina:define-interface :client-form incless-intrinsic:*client*
                            :client-class client
